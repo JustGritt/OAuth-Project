ec484ed23e4f409d24b084cd5fd66651b37d5ed2
Alexis T <alexistan9712@gmail.com>
2022-06-21 10:03:46 +0200

    ⚡ Add Autoloader

--- client/index.php
+++ client/index.php
@@ -1,5 +1,20 @@
 <?php
 
+namespace Sdk;
+
+require "conf.inc.php";
+
+function myAutoloader($class)
+{
+    $class = str_replace("Sdk\\","",$class);
+    $class = str_replace("\\", "/",$class);
+    if(file_exists($class.".class.php")){
+        include $class.".class.php";
+    }
+}
+
+spl_autoload_register("Sdk\myAutoloader");
+
 function login()
 {
     $queryParams= http_build_query([
